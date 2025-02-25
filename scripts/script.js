af3a711032b84a35be0d0441fdfd14d87e1e2a4c
Anton Khleb <toni.farsh@icloud.com>
2025-02-25 16:04:54 +0500

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -68,15 +68,7 @@ var swiper = new Swiper('.swiper-container', {
 
 });
 
-// header scroll
-window.addEventListener('scroll', function () {
-    const header = document.querySelector('.header');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
+
 
 // menu mobil
 
