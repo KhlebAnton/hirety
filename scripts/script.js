5efea86e8d41650ee522068393f1559db14ce93c
Anton Khleb <toni.farsh@icloud.com>
2025-02-26 14:49:58 +0500

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -34,6 +34,12 @@ phoneInput.addEventListener('input', function (e) {
         if (value.length > 9) {
             formattedValue += '-' + value.substring(9, 11);
         }
+
+        // Проверка на полноту номера
+        if (value.length === 11) {
+            phoneInput.classList.remove('error-outline');
+            phoneError.style.display = 'none';
+        } 
     }
 
     e.target.value = formattedValue;
@@ -48,8 +54,16 @@ phoneInput.addEventListener('keydown', function (e) {
 // form imitation
 document.getElementById('feedbackForm').addEventListener('submit', function (event) {
     event.preventDefault();
-    alert('Форма отправлена! (имитация)');
-    closeModal()
+    const phoneValue = phoneInput.value.replace(/\D/g, '');
+    if (phoneValue.length === 11) {
+        alert('Форма отправлена! (имитация)');
+        phoneInput.classList.remove('error-outline');
+        phoneError.style.display = 'none';
+        closeModal(); // Предполагается, что эта функция существует
+    } else {
+        phoneInput.classList.add('error-outline');
+        phoneError.style.display = 'inline';
+    }
 });
 
 // swiper
@@ -75,6 +89,20 @@ var swiper = new Swiper('.swiper-container', {
 const mobilMenu = document.querySelector('.menu__mobile');
 const menuBtn = document.getElementById('menuBtn');
 const menuIcon = document.getElementById('menuIcon');
+const menuLinks = document.querySelectorAll('.btn_menu-mobile');
+
+menuLinks.forEach((link)=> {
+    link.addEventListener('click', ()=> {
+        menuLinks.forEach((e) => e.classList.remove('btn_primary'));
+        link.classList.add('btn_primary');
+        
+            mobilMenu.classList.add('hidden');
+            menuIcon.innerHTML = `
+                <path d="M4 18C3.71667 18 3.47917 17.9042 3.2875 17.7125C3.09583 17.5208 3 17.2833 3 17C3 16.7167 3.09583 16.4792 3.2875 16.2875C3.47917 16.0958 3.71667 16 4 16H20C20.2833 16 20.5208 16.0958 20.7125 16.2875C20.9042 16.4792 21 16.7167 21 17C21 17.2833 20.9042 17.5208 20.7125 17.7125C20.5208 17.9042 20.2833 18 20 18H4ZM4 13C3.71667 13 3.47917 12.9042 3.2875 12.7125C3.09583 12.5208 3 12.2833 3 12C3 11.7167 3.09583 11.4792 3.2875 11.2875C3.47917 11.0958 3.71667 11 4 11H20C20.2833 11 20.5208 11.0958 20.7125 11.2875C20.9042 11.4792 21 11.7167 21 12C21 12.2833 20.9042 12.5208 20.7125 12.7125C20.5208 12.9042 20.2833 13 20 13H4ZM4 8C3.71667 8 3.47917 7.90417 3.2875 7.7125C3.09583 7.52083 3 7.28333 3 7C3 6.71667 3.09583 6.47917 3.2875 6.2875C3.47917 6.09583 3.71667 6 4 6H20C20.2833 6 20.5208 6.09583 20.7125 6.2875C20.9042 6.47917 21 6.71667 21 7C21 7.28333 20.9042 7.52083 20.7125 7.7125C20.5208 7.90417 20.2833 8 20 8H4Z" fill="#2B2B2B"/>
+            `;
+        
+    }) 
+})
 
 function toggleMobilMenu(event) {
     event.stopPropagation(); // Останавливаем всплытие события
